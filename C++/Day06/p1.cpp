fce29e6611108cd95c63c6a9bc3d885cfe326960
stevenStrij <ststrijakov@gmail.com>
2016-12-07 18:22:23 +1100

    Finished day6 in c++

---
+++ C++/Day06/p1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+int main() {
+  std::vector<std::map<char, int>> freqs{};
+  std::string line{};
+
+  while (std::getline(std::cin, line)) {
+    for (int i = 0; i < line.length(); ++i) {
+      if (freqs.size() < i + 1) freqs.push_back(std::map<char, int>());
+      freqs[i][line[i]]++;
+    }
+  }
+
+  std::cout << "Done" << std::endl;
+
+  for (std::map<char, int> m : freqs) {
+    auto max = std::max_element(m.begin(), m.end(),
+        [] (const auto& a, const auto& b) {
+          return a.second < b.second;
+        });
+
+    std::cout << max->first;
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
