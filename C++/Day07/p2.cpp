658932d7cd20960499fe1d2bdfae3e44627db318
stevenStrij <ststrijakov@gmail.com>
2016-12-10 14:35:14 +1100

    moar work

--- C++/Day07/p2.cpp
+++ C++/Day07/p2.cpp
@@ -1,38 +1,48 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 int main() {
   std::string line;
   int c = 0;
 
-  std::map<std::string, bool> inBrackets{};
-  std::map<std::string, bool> outBrackets{};
 
   while (std::getline(std::cin, line)) {
     char ch;
     bool inBrackets = false;
     bool valid = false;
 
+    // True if in brackets
+    std::map<std::string, bool> seqs{};
+
+    std::cout << line << std::endl;
     for (int i = 0; i < line.length() - 2; ++i) {
       ch = line[i];
       if (ch == '[') inBrackets = true;
       if (ch == ']') inBrackets = false;
 
       if (line[i] == line[i + 2] && line[i] != line[i + 1]) {
-        if (inBrackets) {
-          valid = false;
-          break;
+        std::string sub(line.begin() + i, line.begin() + i + 3);
+        std::string rev{sub[1], sub[0], sub[1]};
+        std::cout << sub << std::endl;
+
+        if (seqs.find(rev) != seqs.end()) {
+          if (seqs[rev] == !inBrackets) {
+            std::cout << "Found under " << sub << " -- " << rev << std::endl;
+            valid = true;
+            break;
+          }
         }
-        valid = true;
+        seqs[sub] = inBrackets;
       }
     }
 
     if (valid) {
-      std::cout << "Valid: " << line << std::endl;
       ++c;
-      std::cout << std::endl;
+      std::cout << line << " is valid" << std::endl;
     }
+    std::cout << "------------------------"<< std::endl;
   }
 
   std::cout << c << std::endl;
